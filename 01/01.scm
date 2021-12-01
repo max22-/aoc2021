ce4e145b6171cbe444af537d7df84f9f0b9a5bae
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2021-12-01 10:54:29 +0100

    Day 1 part 2

--- 01/01.scm
+++ 01/01.scm
@@ -9,7 +9,7 @@
       '()
       (cons (list (car a) (car b)) (zip (cdr a) (cdr b)))))
 
-(define (solution input)
+(define (solution1 input)
   (apply +
 	 (map (lambda (e)
 		(if (< (car e) (cadr e))
@@ -17,5 +17,25 @@
 		    0))
 	      (zip input (cdr input)))))
 
+(display "Solution 1: ")
 (display
- (solution (call-with-input-file "input.txt" read-number)))
+ (solution1 (call-with-input-file "input.txt" read-number)))
+(newline)
+
+(define (zipWith3 f a b c)
+  (if (or (null? a) (null? b) (null? c))
+      '()
+      (cons (f (car a) (car b) (car c))
+	    (zipWith3 f (cdr a) (cdr b) (cdr c)))))
+
+(define example (call-with-input-file "example.txt" read-number))
+
+(define (apply-window input)
+  (zipWith3 + input (cdr input) (cddr input)))
+
+(define (solution2 input)
+  (solution1 (apply-window input)))
+
+(display "Solution2: ")
+(display (solution2 (call-with-input-file "input.txt" read-number)))
+(newline)
