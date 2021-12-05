626ea81a59611276c8463f2e69bd12aa082e8e10
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2021-12-05 09:57:11 +0100

    Forgot to commit formatted code of yesterday

--- 04/04.c
+++ 04/04.c
@@ -1,8 +1,8 @@
+#include <ctype.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdarg.h>
-#include <ctype.h>
 
 typedef unsigned int hptr_t;
 
@@ -14,189 +14,179 @@ unsigned int heap_mark[65536] = {0};
 typedef unsigned int array;
 
 #define dim0(a) (heap[(a)])
-#define dim1(a) (heap[(a)+1])
-#define data_idx(a) ((a)+2)
-#define at(a, x, y) (heap[data_idx(a) + (y) * dim0(a) + (x)])
+#define dim1(a) (heap[(a) + 1])
+#define data_idx(a) ((a) + 2)
+#define at(a, x, y) (heap[data_idx(a) + (y)*dim0(a) + (x)])
 #define mark_offset(a, o) (heap_mark[data_idx(a) + (o)])
-#define mark_xy(a, x, y) mark_offset(a, (y) * dim0(a) + (x))
+#define mark_xy(a, x, y) mark_offset(a, (y)*dim0(a) + (x))
 
-
-void display(array a)
-{
+void display(array a) {
   int i, j;
   unsigned int d0 = dim0(a), d1 = dim1(a);
   printf("(%u, %u)\n", d0, d1);
-  for(j = 0; j < d1; j++) {
-    for(i = 0; i < d0; i++) {
+  for (j = 0; j < d1; j++) {
+    for (i = 0; i < d0; i++) {
       /*printf("%2u ", heap[data_idx(a) + j * dim0(a) + i]);*/
       printf("%2u", at(a, i, j));
-      if(mark_xy(a, i, j))
-	printf("* ");
+      if (mark_xy(a, i, j))
+        printf("* ");
       else
-	printf("  ");
+        printf("  ");
     }
     printf("\n");
   }
   printf("\n");
 }
 
-int check(array a)
-{
+int check(array a) {
   int i, j, result;
-  for(j = 0; j < dim1(a); j++) {
+  for (j = 0; j < dim1(a); j++) {
     result = 1;
-    for(i = 0; i < dim0(a); i++) {
-      if(!mark_xy(a, i, j))
-	result = 0;
+    for (i = 0; i < dim0(a); i++) {
+      if (!mark_xy(a, i, j))
+        result = 0;
     }
-    if(result)
+    if (result)
       return 1;
   }
 
-  for(i = 0; i < dim0(a); i++) {
+  for (i = 0; i < dim0(a); i++) {
     result = 1;
-    for(j = 0; j < dim1(a); j++) {
-      if(!mark_xy(a, i, j))
-	 result = 0;
+    for (j = 0; j < dim1(a); j++) {
+      if (!mark_xy(a, i, j))
+        result = 0;
     }
-    if(result)
+    if (result)
       return 1;
   }
- 
+
   return 0;
 }
 
-void dump_heap()
-{
+void dump_heap() {
   int i;
   printf("heap:\n");
-  for(i=0; i< hptr; i++)
+  for (i = 0; i < hptr; i++)
     printf("%u ", heap[i]);
   printf("\n");
 }
 
-void dump_array(array a)
-{
+void dump_array(array a) {
   int i;
   printf("array:\n");
-  for(i = 0; i < dim0(a) * dim1(a); i++)
-    printf("%u ", heap[data_idx(a)+i]);
+  for (i = 0; i < dim0(a) * dim1(a); i++)
+    printf("%u ", heap[data_idx(a) + i]);
   printf("\n");
 }
 
-int main()
-{
+int main() {
   array a[1000] = {0};
   unsigned int acnt = 0, x, y, i, j, k, l, m, score;
   char c;
 
   unsigned int winners[1000] = {0}, winners_count, loser;
 
-  
   a[acnt] = hptr;
-  hptr+=2;
-  x = 0; y=0;
+  hptr += 2;
+  x = 0;
+  y = 0;
   do {
     scanf("%u", &heap[hptr++]);
     x++;
-  } while((c = getchar()) == ',');
+  } while ((c = getchar()) == ',');
   dim0(a[acnt]) = x;
   dim1(a[acnt]) = 1;
   acnt++;
 
-  while(!feof(stdin)) {
-    while(!isdigit(c = getchar()) && !feof(stdin));
-    if(feof(stdin))
+  while (!feof(stdin)) {
+    while (!isdigit(c = getchar()) && !feof(stdin))
+      ;
+    if (feof(stdin))
       break;
     ungetc(c, stdin);
     a[acnt] = hptr;
-    hptr+=2;
-    y=0;
+    hptr += 2;
+    y = 0;
     do {
       x = 0;
       do {
-	scanf("%u", &heap[hptr++]);
-	x++;
-      } while((c=getchar()) == ' ');
-      if(y == 0) {
-;
-	dim0(a[acnt]) = x;
+        scanf("%u", &heap[hptr++]);
+        x++;
+      } while ((c = getchar()) == ' ');
+      if (y == 0) {
+        ;
+        dim0(a[acnt]) = x;
       }
-      if(c != -1)
-	y++;
-      if(c != '\n') {
-	if(c == -1)
-	  break;
-	fprintf(stderr, "Invalid input\n");
-	exit(1);
+      if (c != -1)
+        y++;
+      if (c != '\n') {
+        if (c == -1)
+          break;
+        fprintf(stderr, "Invalid input\n");
+        exit(1);
       }
-      if((c=getchar()) == '\n')
-	break;
+      if ((c = getchar()) == '\n')
+        break;
       else
-	ungetc(c, stdin);
-    } while(1);
+        ungetc(c, stdin);
+    } while (1);
     dim1(a[acnt]) = y;
     acnt++;
-
-    
   }
-  for(i =0; i < acnt; i++)
+  for (i = 0; i < acnt; i++)
     display(a[i]);
   /*dump_array(a[3]);
     dump_heap();*/
 
-  
-  for(i = 0; i < dim0(a[0]); i++) {
+  for (i = 0; i < dim0(a[0]); i++) {
     /*printf("turn number %u: %u\n", i, at(a[0], i, 0));*/
-    for(j = 1; j < acnt; j++) {
-      for(k = 0; k < dim1(a[j]); k++) {
-	for(l = 0; l < dim0(a[j]); l++) {
-	  if(at(a[j], k, l) == at(a[0], i, 0)) {
-	    mark_xy(a[j], k, l) = 1;
-	  }
-	}
+    for (j = 1; j < acnt; j++) {
+      for (k = 0; k < dim1(a[j]); k++) {
+        for (l = 0; l < dim0(a[j]); l++) {
+          if (at(a[j], k, l) == at(a[0], i, 0)) {
+            mark_xy(a[j], k, l) = 1;
+          }
+        }
       }
-      
-      if(check(a[j]))
-	winners[j] = 1;
+
+      if (check(a[j]))
+        winners[j] = 1;
 
       winners_count = 0;
-      for(m = 1; m < acnt; m++)
-	if(winners[m])
-	  winners_count++;
-      if(winners_count == acnt - 1)
-	goto lose;
-      if(check(a[j]))
-	winners[j] = 1;
+      for (m = 1; m < acnt; m++)
+        if (winners[m])
+          winners_count++;
+      if (winners_count == acnt - 1)
+        goto lose;
+      if (check(a[j]))
+        winners[j] = 1;
     }
-    
   }
-  
+
   /*for(m =0; m < acnt; m++)
     display(a[m]);
     sleep(1);*/
-  
 
   fprintf(stderr, "No single solution :(");
   exit(1);
- lose:
+lose:
 
-  for(loser = 1; loser < acnt; loser++)
-    if(!winners[loser])
+  for (loser = 1; loser < acnt; loser++)
+    if (!winners[loser])
       break;
-  printf("board %d loses after %u turns. Last number : %u\n", loser, i, at(a[0], i, 0));
+  printf("board %d loses after %u turns. Last number : %u\n", loser, i,
+         at(a[0], i, 0));
   display(a[j]);
 
   score = 0;
-  for(k = 0; k < dim1(a[j]); k++)
-    for(l = 0; l < dim0(a[j]); l++)
-      if(!mark_xy(a[j], k, l))
-	score += at(a[j], k, l);
+  for (k = 0; k < dim1(a[j]); k++)
+    for (l = 0; l < dim0(a[j]); l++)
+      if (!mark_xy(a[j], k, l))
+        score += at(a[j], k, l);
 
   score *= at(a[0], i, 0);
 
   printf("score : %u\n", score);
 
-  
   return 0;
 }
