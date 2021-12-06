21c926430f8a8982370b06664780bb7ac733d531
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2021-12-06 11:42:43 +0100

    Day 6 part 2

--- 06/06.hs
+++ 06/06.hs
@@ -4,22 +4,26 @@ import Control.Monad
 sepBy :: Parser a -> Parser b -> Parser [a]
 sepBy p sep = liftM2 (:) p (many (sep >> p))
 
-births :: [Int] -> [Int]
-births xs = replicate (length (filter (==0) xs)) 8
+-- Lanternfishes
+--  0  1  2  3  4  6  7  8     <- counters (index of the list)
+-- [0, 1, 1, 2, 1, 0, 0, 0]    <- how many have this counter value
 
-dec_reset :: [Int] -> [Int]
-dec_reset = map (\x -> if x == 0 then 6 else pred x)
+modify :: (a -> a) -> Int -> [a] -> [a]
+modify f 0 (x:xs) = f x : xs
+modify f n (x:xs) = x : modify f (n-1) xs
+
+initial :: [Int] -> [Int]
+initial = foldr (modify succ) (replicate 9 0)
 
 nextgen :: [Int] -> [Int]
-nextgen xs = dec_reset xs ++ births xs
+nextgen (x:xs) = modify (+x) 6 xs ++ [x]
 
-solution :: [Int] -> Int
-solution = length . last . take (80+1) . iterate nextgen
+solution n = sum . last . take (n + 1) . iterate nextgen . initial
 
 main :: IO ()
 main = do
      input <- getContents
      case parse (sepBy natural (char ',')) input of
 	 [] -> error "Input error"
-	 [(l, "")] -> print $ solution l
+	 [(l, "")] -> print $ solution 256 l
 	 _ -> error "Input error"
